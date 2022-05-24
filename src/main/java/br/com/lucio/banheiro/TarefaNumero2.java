1756e278b415f0f097813971351cf7702e945514
Rodrigo Lúcio <luciodigo@gmail.com>
2022-05-23 23:07:36 -0300

    Bloqueio de banheiro por convidado

---
+++ src/main/java/br/com/lucio/banheiro/TarefaNumero2.java
@@ -0,0 +1,16 @@
+package br.com.lucio.banheiro;
+
+public class TarefaNumero2 implements Runnable {
+
+	private Banheiro banheiro;
+	
+	public TarefaNumero2(Banheiro banheiro) {
+		this.banheiro = banheiro;
+	}
+
+	@Override
+	public void run() {
+		banheiro.fazNumero2();
+	}
+
+}
